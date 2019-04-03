2cf0a5d742e26148a1396e35fb8c1e7055969929
Danilo <mdanilor@protonmail.com>
2019-04-03 16:16:54 -0300

    Fixed db functions

--- Session-fixation/Session-fixation.py
+++ Session-fixation/Session-fixation.py
@@ -1,16 +1,21 @@
 import os
 from flask import Flask, request, url_for, render_template, redirect
-
+from config import sqlite
+from models import sfmodel
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.config['DEBUG'] = True
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def start():
     return render_template("index.html")
 
+@app.route('/home/')
+@app.route('/home/<sessid>')
+def home():
+    
 
-@app.route("/home", methods=['POST'])
+@app.route("/login", methods=['POST'])
 def home():
     sizeImg = request.form['size']
     os.system('convert static/img/bones.png -resize '+sizeImg+'% static/img/bones.png')
--- Session-fixation/models/sfmodel.py
+++ Session-fixation/models/sfmodel.py
@@ -6,16 +6,15 @@ class Users:
     def authenticateUser(self, userName, password, sessionToken):
         db = database_con()
         hashedPw = hashlib.sha1(password).hexdigest()
-        sql = ''' UPDATE users SET SessionToken=? WHERE UserName=? AND Password=? '''
+        sql = ''' UPDATE users SET SessionToken=? WHERE UserName=? AND Password=?; '''
         params = (sessionToken, userName, hashedPw)
         cur = db.cursor()
         cur.execute(sql, params)
-        print (sql, params)
-        #db.commit()
+        db.commit()
 
     def getUserFromSessionToken(self, sessionToken):
         db = database_con()
-        sql = ''' SELECT userName FROM users WHERE SessionToken=? '''
+        sql = ''' SELECT userName FROM users WHERE SessionToken=?; '''
         params = (sessionToken, )
         cur = db.cursor()
         cur.execute(sql, params)
